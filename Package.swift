47c1719b4285af593bf99f373963d5c788a3bbec
Frizlab <fload@me.com>
2022-12-23 20:41:42 +0200

    Add minimum OS requirements for the package

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
 	name: "DependencyInjection",
+	platforms: [
+		.macOS(.v10_15),
+		.tvOS(.v13),
+		.iOS(.v13),
+		.watchOS(.v6)
+	],
 	products: [.library(name: "DependencyInjection", targets: ["DependencyInjection"])],
 	targets: [.target(name: "DependencyInjection")]
 )
