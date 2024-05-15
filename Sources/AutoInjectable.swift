bf97ad0bb8abf8b31e862b50cc4b1838a7074c0f
Frizlab <fload@me.com>
2024-05-15 02:43:25 +0200

    Remove optionality

--- Sources/AutoInjectable.swift
+++ Sources/AutoInjectable.swift
@@ -5,14 +5,14 @@ import Foundation
 public protocol AutoInjectable {
 	
 	associatedtype InjectedType
-	static var injectionPath: WritableKeyPath<ServiceContext, InjectedType?> {get}
+	static var injectionPath: WritableKeyPath<ServiceContext, InjectedType> {get}
 	
 }
 
 
 public extension Injected where Key : AutoInjectable, Key.InjectedType == Key {
 	
-	static var value: Key! {
+	static var value: Key {
 		get {Self.value(Key.injectionPath)}
 		set {Self.setValue(newValue, for: Key.injectionPath)}
 	}
--- Sources/Injected.swift
+++ Sources/Injected.swift
@@ -11,22 +11,22 @@ private var context = ServiceContext.topLevel
 @propertyWrapper
 public struct Injected<Key> {
 	
-	public static func value(_ keyPath: KeyPath<ServiceContext, Key?>) -> Key! {
+	public static func value(_ keyPath: KeyPath<ServiceContext, Key>) -> Key {
 		context[keyPath: keyPath]
 	}
 	
-	public static func setValue(_ value: Key, for keyPath: WritableKeyPath<ServiceContext, Key?>) {
+	public static func setValue(_ value: Key, for keyPath: WritableKeyPath<ServiceContext, Key>) {
 		context[keyPath: keyPath] = value
 	}
 	
-	public var wrappedValue: Key! {
+	public var wrappedValue: Key {
 		Self.value(keyPath)
 	}
 	
-	public init(_ keyPath: KeyPath<ServiceContext, Key?>) {
+	public init(_ keyPath: KeyPath<ServiceContext, Key>) {
 		self.keyPath = keyPath
 	}
 	
-	private let keyPath: KeyPath<ServiceContext, Key?>
+	private let keyPath: KeyPath<ServiceContext, Key>
 	
 }
