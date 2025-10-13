b4fa302bb96c3e9b91efa97aceeee5c1e6a0b254
Frizlab <fload@me.com>
2025-10-13 03:54:27 +0200

    Dummy addition of commented swift-syntax version

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
 		.package(url: "https://github.com/apple/swift-service-context.git", from: "1.0.0"),
 		/* TODO: CI should test the package w/ all of the major versions we support of swift-syntax specified explicitly. */
 		.package(url: "https://github.com/swiftlang/swift-syntax.git",      "509.0.0"..<"603.0.0"),
+//		.package(url: "https://github.com/swiftlang/swift-syntax.git",      from: "602.0.0"),
 //		.package(url: "https://github.com/swiftlang/swift-syntax.git",      from: "601.0.0"),
 //		.package(url: "https://github.com/swiftlang/swift-syntax.git",      from: "600.0.0"),
 //		.package(url: "https://github.com/swiftlang/swift-syntax.git",      from: "510.0.0"),
